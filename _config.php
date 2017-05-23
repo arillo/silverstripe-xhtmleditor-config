c311c072ea014e89d3bc7dfd574c12cacf1dc492
DM <dm@arillo.net>
2017-05-23 10:59:31 +0200

    added identifier

--- _config.php
+++ _config.php
@@ -1,8 +1,9 @@
 <?php
+$identifier = Config::inst()->get('XHTMLEditorConfig','identifier');
 $buttons = Config::inst()->get('XHTMLEditorConfig','buttons');
 $options = Config::inst()->get('XHTMLEditorConfig','options');
 
-$htmlConfig = HtmlEditorConfig::get('cms')
+$htmlConfig = HtmlEditorConfig::get($identifier)
 	->setButtonsForLine(1, $buttons[1])
 	->setButtonsForLine(2, $buttons[2])
 	->setButtonsForLine(3, $buttons[3])
